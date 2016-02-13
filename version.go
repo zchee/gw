1e3fa16df176cd65afe36a8794e1c04e4769e9df
Koichi Shiraishi <k@zchee.io>
2016-02-13 13:33:04 +0900

    Add version file
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

---
+++ version.go
@@ -0,0 +1,9 @@
+package main
+
+var (
+	// Version should be updated by hand at each release
+	Version string = "0.0.1-dev"
+
+	// GitCommit will be overwritten automatically by the build system
+	GitCommit string = "HEAD"
+)
